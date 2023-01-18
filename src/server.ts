70af7a2f095a503c8684ebe6624dffddd24d3cf8
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-18 20:52:20 -0300

    insert server.js

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import {Request, Response} from 'express';
 import express from 'express';
 
-
 const app = express()
 
-app.get("/", (Request, response)=> {
-    return response.json({message: "Helo World ignite"})
-}); // usando metodo de requisicao get 
+app.listen()
