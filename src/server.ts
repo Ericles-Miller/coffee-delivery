ad90ea3b2cfe991060d5c204f400eca90f446b4b
Ericles-Miller <ericlesmiller15@gmail.com>
2023-01-18 21:01:59 -0300

    insert types on project

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
-import {Request, Response} from 'express';
 import express from 'express';
 
 const app = express()
 
 app.listen()
+
+app.get("/", (request,response) =>{
+	return response.json({message:"Hello World"});
+});
+
+app.listen(3333)
