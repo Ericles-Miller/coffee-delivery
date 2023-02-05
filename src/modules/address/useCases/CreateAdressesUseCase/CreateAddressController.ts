75c91ada35edc0be6e1593a3ff233addfc83b4d9
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-05 14:59:23 -0300

    insert controller on aplication to prepare insert on database the entity Address

---
+++ src/modules/address/useCases/CreateAdressesUseCase/CreateAddressController.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateAddressUseCase } from './CreateAddressUseCase';
+
+class CreateAddressController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const {
+      cep, number, uf, street, district, complement, city,
+    } = request.body;
+
+    const createAddressUseCase = container.resolve(CreateAddressUseCase);
+
+    const address = await createAddressUseCase.execute({
+      cep,
+      number,
+      uf,
+      street,
+      district,
+      complement,
+      city,
+    });
+
+    return response.status(200).json(address);
+  }
+}
+
+export { CreateAddressController };
---
+++ src/shared/Container/index.ts
@@ -0,0 +1,9 @@
+import { container } from 'tsyringe';
+
+import { AddressRepository } from '../../modules/address/infra/typeorm/repositories/AddressRepository';
+import { IAddressRepository } from '../../modules/address/Repositories/IAdressesRepository';
+
+container.registerSingleton<IAddressRepository>(
+  'AddressRepository',
+  AddressRepository,
+);
