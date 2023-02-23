655b5e074c680746441d5ee1a8da77aab06033be
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-23 13:58:47 -0300

    att

--- src/shared/http/routes/address.routes.ts
+++ src/shared/http/routes/address.routes.ts
@@ -4,8 +4,6 @@ import { CreateAddressController } from '@modules/address/useCases/CreateAddress
 
 const addressRoutes = Router();
 
-// addressRoutes.post('/create');
-
 const createAddressController = new CreateAddressController();
 
 addressRoutes.post('/', createAddressController.handle);
