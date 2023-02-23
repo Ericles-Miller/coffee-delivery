b53ec8c2b675b541dda09cafc89cf5bcc66b564c
Ericles-Miller <ericlesmiller15@gmail.com>
2023-02-23 17:30:52 -0300

    prepare code to push users

--- src/modules/accounts/infra/typeorm/Entities/User.ts
+++ src/modules/accounts/infra/typeorm/Entities/User.ts
@@ -1,13 +1,21 @@
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('users')
 class User {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   name: string;
+  @Column()
   email: string;
+  @Column()
   password: string;
+  @Column()
   userName: string;
   // avatar: string;
-  // isAdmin: boolean;
+  @Column()
+  isAdmin: boolean;
   // create_at: Date;
 
   constructor() {
--- src/shared/Container/index.ts
+++ src/shared/Container/index.ts
@@ -1,9 +1,16 @@
 import { container } from 'tsyringe';
 
+import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRespository';
+import { IUserRepository } from '@modules/accounts/Repositories/IUserRepository';
 import { AddressRepository } from '@modules/address/infra/typeorm/repositories/AddressRepository';
 import { IAddressRepository } from '@modules/address/Repositories/IAdressesRepository';
 
 container.registerSingleton<IAddressRepository>(
   'AddressRepository',
   AddressRepository,
+);
+
+container.registerSingleton<IUserRepository>(
+  'UsersRepository',
+  UsersRepository,
 );
